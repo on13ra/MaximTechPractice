4bed07eb98fc42dff8f9c54c8e4279953cf646c3
onqura <rflshgbv1@gmail.com>
2023-10-04 20:24:17 +0300

    Задание 4

--- MaximTechPractice/Program.cs
+++ MaximTechPractice/Program.cs
@@ -1,6 +1,6 @@
-﻿using System.Linq;
-START:
-Console.WriteLine("Введите Cтроку на английском языке в нижнем регистре");
+﻿START:
+Console.WriteLine("   Введите Cтроку на английском языке в нижнем регистре");
+Console.Write("   ");
 string line = Console.ReadLine();
 if (line == null || line.Trim() == "") goto START;
 bool result = AlphabetCheck(line);
@@ -11,18 +11,18 @@ string output;
 if (line.Length % 2 == 0)
 {
     Array.Reverse(parts, length / 2, length / 2);
-     output = new string(parts);
-    Console.WriteLine("Вывод: {0}", output);
+    output = new string(parts);
+    Console.WriteLine(" Вывод: {0}", output);
 }
 else
 {
     string part1 = new string(parts);
     Array.Reverse(parts);
     string part2 = new string(parts);
-     output = part1 + part2;
-    Console.WriteLine("Вывод: {0}",output);
+    output = part1 + part2;
+    Console.WriteLine(" Вывод: {0}", output);
 }
-//Помимо обработанной строки, необходимо также возвращать пользователю информацию о том, сколько раз повторялся каждый символ в обработанной строке. 
+//Задание 3 - Помимо обработанной строки, необходимо также возвращать пользователю информацию о том, сколько раз повторялся каждый символ в обработанной строке. 
 var charCounts = output
            .GroupBy(c => c)
            .Select(g => new
@@ -34,25 +34,49 @@ var charCounts = output
 
 if (charCounts.Any())
 {
-    Console.WriteLine("Повторяющиеся символы и их количество:");
+    Console.WriteLine("  Повторяющиеся символы и их количество:");
     foreach (var charCount in charCounts)
     {
-        Console.WriteLine($"{charCount.Character}: {charCount.Count} раз");
+        Console.Write($"  {charCount.Character}: {charCount.Count} раз\t");
     }
 }
+
+// Задание 4 - Необходимо найти в обработанной строке наибольшую подстроку, которая начинается и заканчивается на гласную букву из «aeiouy»
+char[] target = "aeiouy".ToCharArray();
+char[] rebuild2 = output.ToCharArray();
+char[] rebuild2Rev = output.ToCharArray();
+Array.Reverse(rebuild2Rev);
+int first = 0;
+int last = 0;
+for (int i = 0; i < rebuild2.Length; i++)
+    if (target.Contains(rebuild2[i]))
+    {
+        first += i;
+        break;
+    }
+for (int i = 0; i < rebuild2.Length; i++)
+    if (target.Contains(rebuild2Rev[i]))
+    {
+        last += i;
+        break;
+    }
+string substr = output.Substring(first);
+substr = substr.Substring(0, substr.Length - last);
+Console.WriteLine("\n Наибольшая подстрока на гласную: {0}", substr);
 Console.ReadKey();
 
+//Задание 2 - Проверка ввода
 static bool AlphabetCheck(string input)
 {
     string alphabet = "abcdefghijklmnopqrstuvwxyz ";
     input = input.ToLower();
     char[] word = input.ToCharArray();
     string issues = "";
-   
+
     for (int j = 0; j < word.Length; j++)
     {
         string tocheck = word[j].ToString();
-        
+
         if (alphabet.Contains(tocheck))
         {
             continue;
@@ -65,15 +89,8 @@ static bool AlphabetCheck(string input)
 
     if (issues != "")
     {
-       Console.WriteLine("Неподходящие значения: {0}", issues);
+        Console.WriteLine("Неподходящие значения: {0}", issues);
         return false;
     }
-        return true;
-}
-public static class Extensions
-{
-    public static bool find<T>(this T[] array, T target)
-    {
-        return array.Contains(target);
-    }
+    return true;
 }
